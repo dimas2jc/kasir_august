2d7d86a8cfdfc40771e0d25e838c73267146e099
dimas2jc <50562377+dimas2jc@users.noreply.github.com>
2020-08-28 09:54:44 +0700

    first init

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+// User
+
+Route::get('login', function () {
+    return view('Auth.login');
+})->name('login');
+
+Route::get('recovery-password', function () {
+    return view('Auth.recovery-password');
+})->name('recovery-password');
