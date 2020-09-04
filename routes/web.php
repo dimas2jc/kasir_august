6f83c7a82635441c90fbde3a12b766a1943bf15b
dimas2jc <50562377+dimas2jc@users.noreply.github.com>
2020-09-04 10:38:37 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::get('recovery-password', function () {
 // owner
 
 Route::get('/dataPegawai', 'pegawaiController@index');
-Route::post('/dataPegawai', 'pegawaiController@store');
+Route::post('/dataPegawai/insert', 'pegawaiController@store');
 Route::get('editDataPegawai/{id}', 'pegawaiController@show');
 Route::post('editDataPegawai/{id}', 'pegawaiController@update');
 Route::get('/laporan', function () {
